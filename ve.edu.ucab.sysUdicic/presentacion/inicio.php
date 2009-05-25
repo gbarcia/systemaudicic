6d3ffddd7f187bc554e2feaeb182b5896931ca7a
trujillo.jonathan <trujillo.jonathan@f1be6066-4621-11de-b25d-9f9778916a3c>
2009-05-25 23:59:01 +0000

    Interfaz

--- ve.edu.ucab.sysUdicic/presentacion/inicio.php
+++ ve.edu.ucab.sysUdicic/presentacion/inicio.php
@@ -4,15 +4,15 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>sysUDICIC | Inicio</title>
         <link rel="stylesheet" type="text/css" href="css/styleMain.css" />
-        <script src="javascript/jQuery/jQuery-1.3.2.js" type="text/javascript"></script>
-        <script src="javascript/jQuery/menu.js" type="text/javascript"></script>
     </head>
     <body>
         <?php
         include 'header.php';
         include 'menu.php';
         ?>
-        <div class="content">asd</div>
+        <div class="content">
+            contenido
+        </div>
         <?php
         include 'footer.php';
         ?>
