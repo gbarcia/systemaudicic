ceefadd35b89f88e7bb4110c6fab83aa1af4f816
trujillo.jonathan <trujillo.jonathan@f1be6066-4621-11de-b25d-9f9778916a3c>
2009-05-27 01:16:24 +0000

    Interfaz

--- ve.edu.ucab.sysUdicic/presentacion/eventos/eventosParteI.php
+++ ve.edu.ucab.sysUdicic/presentacion/eventos/eventosParteI.php
@@ -7,4 +7,22 @@ function procesarTarea () {
     $objResponse->addAssign("mensaje", "innerHTML", "$mensaje");
     return $objResponse;
 }
+
+function actualizarTarea () {
+    $objResponse = new xajaxResponse();
+    $mensaje = '<div class="exito"><div class="textoMensaje">
+                    Tarea actualizada con éxito
+                </div></div>';
+    $objResponse->addAssign("mensaje", "innerHTML", "$mensaje");
+    return $objResponse;
+}
+
+function procesarInventario () {
+    $objResponse = new xajaxResponse();
+    $mensaje = '<div class="exito"><div class="textoMensaje">
+                    Inventario registrado con éxito
+                </div></div>';
+    $objResponse->addAssign("mensaje", "innerHTML", "$mensaje");
+    return $objResponse;
+}
 ?>
